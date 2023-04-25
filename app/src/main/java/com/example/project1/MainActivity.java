05a90379f435378edef7db584039f2aba9178127
Sethumanikkam <seth.y@dbk.cloud>
2023-04-25 22:11:28 +0530

    onclick listeners added

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.project1;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn = findViewById(R.id.button);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Toast.makeText(getApplicationContext(), "Login Successfull!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
